cbe2db7c404a262266cd6448a1212b637900405c
David wanjohi <davidwagura83@gmail.com>
2024-04-03 17:05:07 +0300

    Add get user tweets,get tweet comments and user tweet functions and api routes

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Tweet;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
 {
+
+    public function home(){
+
+        $tweets =  Tweet::all();
+
+        return response()->json($tweets);
+
+    }
+
+
     public function tweet(Request $request)
     {
         $tweet = new Tweet;
@@ -18,6 +29,18 @@ class TweetController extends Controller
         $tweet->save();
 
         return response()->json($tweet);
+    }
+
+    public function user(Request $request)
+    {
+        $user = new User;
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->email = $request->email;
+        $user->username = $request->username;
+        $user->save();
+
+        return response()->json($user);
     }
 
     public function comment(Request $request)
@@ -37,9 +60,23 @@ class TweetController extends Controller
         return response()->json($tweet);
     }
 
-    public function tweetComments($id)
+    public function comments($id)
     {
         $comment = Comment::where('tweet_id', $id)->get();
         return response()->json($comment);
+    }
+
+    public function userTweets($id)
+    {
+        // $comments = Comment::where( 'user_id', $id)->with( 'user')->get();
+        $comments = Comment::where('tweet_id', $id)->with('tweet')->get();
+
+        return response()->json($comments);
+    }
+    public function tweetComments($id)
+    {
+        //('comment')->function name in model relationship
+        $tweet = Tweet::with('comment')->findOrFail($id);
+        return response()->json($tweet);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TweetController;
-use App\Models\Tweet;
+
+Route::get('/', [TweetController::class, 'home']);
 
 Route::post('/tweet', [TweetController::class, 'tweet']);
+
+Route::post('/user', [TweetController::class, 'user']);
+
 Route::post('/tweet/comment', [TweetController::class, 'comment']);
+
+
+
+
+
 Route::get('/tweet/{id}', [TweetController::class, 'showTweet']);
-Route::get('/comment/{id}', [TweetController::class, 'tweetComments']);
+
+Route::get('/comment/{id}', [TweetController::class, 'comments']);
+
+Route::get('/user/tweets/{id}', [TweetController::class, 'userTweets']);
+
+Route::get('tweet/comments/{id}', [TweetController::class, 'tweetComments']);
