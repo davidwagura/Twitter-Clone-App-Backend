fca75fb6ea311b444ac9721a4ecdd43a8ece8ad5
David wanjohi <davidwagura83@gmail.com>
2024-04-23 11:09:39 +0300

    Create media api route

--- routes/api.php
+++ routes/api.php
@@ -67,3 +67,5 @@ Route::get('/userTweetComments/{user_id}', [TweetController::class, 'getUserTwee
 Route::get('/highlights/{user_id}', [TweetController::class, 'showHighlights']);
 
 Route::get('/articles/{user_id}', [TweetController::class, 'showArticles']);
+
+Route::get('/media/{user_id}', [TweetController::class, 'showMedia']);
