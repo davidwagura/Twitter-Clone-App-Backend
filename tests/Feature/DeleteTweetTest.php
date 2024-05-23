16368a02920b8b9575550d2721f6c15dc220f377
David wanjohi <davidwagura83@gmail.com>
2024-05-23 15:56:38 +0300

    Create delete tweet test

---
+++ tests/Feature/DeleteTweetTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Tweet;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DeleteTweetTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+
+    public function test_delete_tweet(): void
+    {
+        $tweet = Tweet::create([
+
+            'id' => 1,
+
+            'body' => 'Tweet created successfully',
+
+            'user_id' => 1
+        ]);
+        
+        $response = $this->delete('/api/tweet/delete/1');
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+
+            'message' => 'Tweet deleted successfully'
+
+        ]);
+    }
+}
