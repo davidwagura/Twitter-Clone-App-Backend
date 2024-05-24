ad5a7168c66f2a0e8c50d0e7fd1b32a00105b3f6
David wanjohi <davidwagura83@gmail.com>
2024-05-24 14:21:49 +0300

    Create user follower test

---
+++ tests/Feature/UserFollowerUnfollowTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserFollowerUnfollowTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+
+    public function test_follower_unfollow(): void
+    {
+        $user = User::create([
+
+            'id' => 1,
+
+            'first_name' => 'John',
+
+            'last_name' => 'Doe',
+
+            'email' => 'Doe@gmail.com',
+
+            'username' => 'Doe',
+
+            'password' => 'John1234'
+        ]);
+
+        $follower = User::create([
+
+            'id' => 1,
+
+            'first_name' => 'Peter',
+
+            'last_name' => 'Smith',
+
+            'email' => 'Peter@gmail.com',
+
+            'username' => 'Smith',
+
+            'password' => 'Peter1234'
+        ]);
+
+        $response = $this->get('/api/unfollow/1/1');
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+
+            'message' => 'Unfollow successful'
+
+        ]);
+
+        $response->assertJsonFragment([
+
+            'id' => 1,
+
+            'first_name' => 'Smith'
+        ]);
+
+    }
+    
+}
