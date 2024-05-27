ad46e91bb72cd935682c665e8e383377edeb5def
David wanjohi <davidwagura83@gmail.com>
2024-05-27 11:37:10 +0300

    Create following users tweets  test

---
+++ tests/Feature/FollowingTweetsTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Tweet;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FollowingTweetsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+
+    public function test_following_tweets(): void
+    {
+        $user1 = User::create([
+
+            'id' => 2,
+
+            'first_name' => 'Kings',
+
+            'last_name' => 'John',
+
+            'email' => 'John@gmail.com',
+
+            'username' => 'John',
+
+            'password' => 'john1234',
+
+
+        ]);
+
+        $user2 = User::create([
+
+            'id' => 2,
+
+            'first_name' => 'Kings',
+
+            'last_name' => 'John',
+
+            'email' => 'John@gmail.com',
+
+            'username' => 'John',
+
+            'password' => 'john1234',
+
+            'followings_id' => $user1->id
+
+        ]);
+
+        $tweet = Tweet::create([
+
+            'id' => 1,
+
+            'body' => 'This is a tweet',
+
+            'user_id' => $user2->id
+            
+        ]);
+
+
+        $response = $this->get('/api/followingTweets/2');
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+
+            'message' => 'Displaying tweets'
+
+        ]);
+
+    }
+}
